db3fe3cb503e40ef79be219a36a1e6ff05d1b634
Claudia <cl.calerolopez@gmail.com>
2018-07-11 12:31:12 +0200

    h2 y img

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+var boton = document.querySelector('.buscador__boton');
+var input = document.querySelector('.buscador__input');
+var ul = document.querySelector('.secondSection__listado');
+
+function buscaSerie() {
+  fetch('http://api.tvmaze.com/search/shows?q=' + input.value)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      var series = json;
+      for (var i = 0; i < series.length; i++) {
+        var li = document.createElement('li');
+        var h2 = document.createElement('h2');
+        var content = document.createTextNode(series[i].show.name);
+        h2.appendChild(content);
+        li.appendChild(h2);
+        ul.appendChild(li);
+        var image = document.createElement('img');
+        li.appendChild(image);
+        image.src = series[i].show.image.medium;
+      }
+    });
+}
+
+boton.addEventListener('click', buscaSerie);
+
